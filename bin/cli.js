97b5a7ab3f37483f09dcda4b9543b91e95ac4802
5Runi <rgeldner@pm.me>
2026-02-09 16:21:25 -0500

    opening a cli.js file

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-import { runGhostbusters } from '../src/core/engine.js';
+import { runGhostbusters } from '../src/core/engine.js';//if we are sticking to ghostbusters (plural) those changes need to be made in core/engine.js as well
 import { formatReport } from '../src/core/format.js'
